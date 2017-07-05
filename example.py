6b46dd925223df3654cb7bd5a550aad36bd6fc33
David <rodavidfernandez@gmail.com>
2017-07-04 22:41:48 -0300

    add library for hxt11

--- example.py
+++ example.py
@@ -10,7 +10,7 @@ def cleanAndExit():
     sys.exit()
 
 hx = HX711(5, 6)
-
+start_value=14226
 # I've found out that, for some reason, the order of the bytes is not always the same between versions of python, numpy and the hx711 itself.
 # Still need to figure out why does it change.
 # If you're experiencing super random values, change these values to MSB or LSB until to get more stable values.
@@ -26,23 +26,24 @@ hx.set_reading_format("LSB", "MSB")
 # and I got numbers around 184000 when I added 2kg. So, according to the rule of thirds:
 # If 2000 grams is 184000 then 1000 grams is 184000 / 2000 = 92.
 #hx.set_reference_unit(113)
-hx.set_reference_unit(92)
+hx.set_reference_unit(663)
 
 hx.reset()
 hx.tare()
+hx.set_offset(start_value)
 
-while True:
+while True:c
     try:
         # These three lines are usefull to debug wether to use MSB or LSB in the reading formats
         # for the first parameter of "hx.set_reading_format("LSB", "MSB")".
         # Comment the two lines "val = hx.get_weight(5)" and "print val" and uncomment the three lines to see what it prints.
         #np_arr8_string = hx.get_np_arr8_string()
         #binary_string = hx.get_binary_string()
-        #print binary_string + " " + np_arr8_string
-        
+#print binary_string + " " + np_arr8_string
+
         # Prints the weight. Comment if you're debbuging the MSB and LSB issue.
         val = hx.get_weight(5)
-        print val
+        print (val - start_value)
 
         hx.power_down()
         hx.power_up()
